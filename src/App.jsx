e983ac70bd0a7c58f43eb42694b7cfb2ded0b936
Tana Ofik <ofik.tana@gmail.com>
2025-05-02 12:37:40 +0100

    some minor housekeeping

--- src/App.jsx
+++ src/App.jsx
@@ -18,46 +18,12 @@ import { getUserSummary, loginUser } from "./backend-functions/useractions-api";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import Demo from "./pages/Demo";
  
- 
 
-// import NotificationsPage from "./pages/NotificationsPage";
 
 function App() {
-  // const isUserAuthenticated = true; // This is a placeholder for now and eventually will be derived from user authentication flow.
-
-  // loginUser({
-  //   "email": "owner1@test.com",
-  //   "password": "Jonathan1@"
-  // })
-
-  // getUserSummary()
 
   return (
-    // <BrowserRouter>
-    //   {isUserAuthenticated ? (
-    //     <Routes>
-    //       <Route path="/" element={<Layout />}>
-    //         <Route index element={<Dashboard />} />
-    //         <Route path="settings" element={<Settings />} />
-    //         <Route path="transactions" element={<AltTransactions />} />
-    //         <Route path="dashboard" element={<Dashboard />} />
-    //         <Route path="agents" element={<Agents />} />
-    //         <Route path="agents/:id" element={<AgentDetails />} />
-    //         <Route path="customers" element={<Customers />} />
-    //         <Route path="customers/:id" element={<CustomerDetails />} />
-    //         <Route path="terminals" element={<Terminals />} />
-    //         <Route path="disputes" element={<Disputes />} />
-    //         <Route path="settings" element={<Settings />} />
-    //         {/* <Route path="notifications" element={<NotificationsPage />} /> */}
-    //       </Route>
-    //     </Routes>
-    //   ) : (
-    //     <Routes>
-    //       <Route path="/" element={<LandingPage />}></Route>
-    //       <Route path="/auth" element={<HomePage />}></Route>
-    //     </Routes>
-    //   )}
-    // </BrowserRouter>
+
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<RequireAuth />}>
@@ -85,7 +51,6 @@ function App() {
             element={<ForgotPasswordPage />}
           ></Route>
           <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/auth" element={<Demo />} />
         </Routes>
     </BrowserRouter>
   );
